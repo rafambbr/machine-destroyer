50c7768180fa43e640a3331ff347ea321813cfaa
Bruno Pedroso <pedroso.bruno@gmail.com>
2017-10-30 15:00:34 -0200

    Clean process

--- src/main/java/com/destroyer/machinedestroyer/resource/DestroyController.java
+++ src/main/java/com/destroyer/machinedestroyer/resource/DestroyController.java
@@ -1,3 +1,12 @@
+/*
+* Copyright 2017 Pernambucanas S.A.
+*************************************************************
+*Nome     : DestroyController.java
+*Autor    : brunopedroso
+*Data     : 30/10/2017
+*Empresa  : Platform Builders
+*************************************************************
+*/
 package com.destroyer.machinedestroyer.resource;
 
 import static java.lang.String.join;
@@ -8,19 +17,20 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/destroy")
 public class DestroyController {
 
-	private static final Map<Long, String> map = new HashMap<Long, String>();
+	private static Map<Long, String> map = new HashMap<Long, String>();
 	private static long memoryCount = 0;
 
 	@Value("${app.memory.text}")
 	private String memoryText;
 
-	@GetMapping("/memory")
-	public String destroyMemory(int power) {
+	@GetMapping("/memory/allocate/{power}")
+	public String destroyMemory(@PathVariable int power) {
 
 		if(power <= 0){
 			power = 1000;
@@ -37,6 +47,23 @@ public class DestroyController {
 						+ ", Used: " + used
 						+ ", Free: " + free;
 	}
+	
+    @GetMapping("/memory/clean")
+    public String clearMemory() {
+        memoryCount = 0;
+        map = null;
+        map = new HashMap<Long, String>();
+
+        Runtime rt = Runtime.getRuntime();
+        long total = rt.totalMemory();
+        long free = rt.freeMemory();
+        long used = total - free;
+
+        return "#" + memoryCount++
+                        + ", Total: " + total
+                        + ", Used: " + used
+                        + ", Free: " + free;
+    }
 
 	@GetMapping("/cpu")
 	public String destroyCpu() {
