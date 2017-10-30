1ba2486df1154593e5ea2ad220ab28d9dc793579
Rafael Camargo <rafael_camargo@carrefour.com>
2017-10-30 11:39:29 -0200

    Destroy Memory

--- src/main/java/com/destroyer/machinedestroyer/App.java
+++ src/main/java/com/destroyer/machinedestroyer/App.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MachineDestroyerApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MachineDestroyerApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 }
--- src/main/java/com/destroyer/machinedestroyer/resource/DestroyController.java
+++ src/main/java/com/destroyer/machinedestroyer/resource/DestroyController.java
@@ -1,18 +1,47 @@
 package com.destroyer.machinedestroyer.resource;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/destroy")
 public class DestroyController {
 
+	private static final Map map = new HashMap();
+	private static long memoryCount = 0;
+
+	@Value("${app.memory.text}")
+	private String memoryText;
+
 	@GetMapping("/memory")
-	public String destroyMemory() {
-		return "memory";
+	public String destroyMemory(int power) {
+
+		if(power <= 0){
+			power = 10000;
+		}
+		map.put(memoryCount,
+						String.join("",
+						Collections.nCopies(power, memoryText)));
+
+		Runtime rt = Runtime.getRuntime();
+		long total = rt.totalMemory();
+		long free = rt.freeMemory();
+		long used = total - free;
+
+		return "#" + memoryCount++
+						+ ", Total: " + total
+						+ ", Used: " + used
+						+ ", Free: " + free;
 	}
 
 	@GetMapping("/cpu")
 	public String destroyCpu() {
+
 		return "cpu";
 	}
 }
+
