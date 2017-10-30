9694f3da8727a6963f4de20bb7df1ebdcd83dd33
brunopedrosoc4 <bruno_pedroso@carrefour.com>
2017-10-30 10:39:10 -0200

    Create project

---
+++ src/test/java/com/destroyer/machinedestroyer/MachineDestroyerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.destroyer.machinedestroyer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MachineDestroyerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
