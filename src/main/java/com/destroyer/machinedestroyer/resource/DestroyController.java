6b93cdf8d96d6f0f1680cb93a3f6e039c8f16b7b
Bruno Pedroso <pedroso.bruno@gmail.com>
2017-10-31 03:00:30 -0200

    Add defaults if text doenst exists

--- src/main/java/com/destroyer/machinedestroyer/resource/DestroyController.java
+++ src/main/java/com/destroyer/machinedestroyer/resource/DestroyController.java
@@ -1,3 +1,10 @@
+/*
+*************************************************************
+*Nome     : DestroyController.java
+*Autor    : brunopedroso
+*Data     : 31/10/2017
+*************************************************************
+*/
 package com.destroyer.machinedestroyer.resource;
 
 import static java.lang.String.join;
@@ -7,6 +14,7 @@ import java.math.BigDecimal;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,79 +25,84 @@ import com.destroyer.machinedestroyer.service.MonteCarloPI;
 
 @RestController("/destroy")
 public class DestroyController {
+  
+  private static Map<Long, String> map = new HashMap<Long, String>();
+  private static long memoryCount = 0;
+  private final static BigDecimal POINTS = new BigDecimal("200000000");
 
-	private static Map<Long, String> map = new HashMap<Long, String>();
-	private static long memoryCount = 0;
-	private final static BigDecimal POINTS = new BigDecimal("200000000");
+  @Value("${app.memory.text}")
+  private String memoryText;
 
-	@Value("${app.memory.text}")
-	private String memoryText;
+  @GetMapping("/memory/{power}")
+  public String destroyMemory(@PathVariable int power) {
+      if(power <= 0)
+          power = 1000;
 
-	@GetMapping("/memory/{power}")
-	public String destroyMemory(@PathVariable int power) {
-		if(power <= 0){
-			power = 1000;
-		}
-		map.put(memoryCount, join("", nCopies(power, memoryText)));
+      if(memoryText==null) {
+        StringBuilder sb = new StringBuilder();
+        IntStream.range(0, 5000).forEach(i -> sb.append("0"));
+        memoryText = sb.toString();
+      }
 
-		Runtime rt = Runtime.getRuntime();
-		long total = rt.totalMemory();
-		long free = rt.freeMemory();
-		long used = total - free;
+      map.put(memoryCount, join("", nCopies(power, memoryText)));
 
-		return "#" + memoryCount++
-						+ ", Total: " + total
-						+ ", Used: " + used
-						+ ", Free: " + free;
-	}
-	
-    @GetMapping("/memory/clean")
-    public String clearMemory() {
-        memoryCount = 0;
-        map = null;
-        map = new HashMap<Long, String>();
+      Runtime rt = Runtime.getRuntime();
+      long total = rt.totalMemory();
+      long free = rt.freeMemory();
+      long used = total - free;
 
-        Runtime rt = Runtime.getRuntime();
-        long total = rt.totalMemory();
-        long free = rt.freeMemory();
-        long used = total - free;
+      return "#" + memoryCount++
+                      + ", Total: " + total
+                      + ", Used: " + used
+                      + ", Free: " + free;
+  }
+  
+  @GetMapping("/memory/clean")
+  public String clearMemory() {
+      memoryCount = 0;
+      map = null;
+      map = new HashMap<Long, String>();
 
-        return "#" + memoryCount++
-                        + ", Total: " + total
-                        + ", Used: " + used
-                        + ", Free: " + free;
-    }
+      Runtime rt = Runtime.getRuntime();
+      long total = rt.totalMemory();
+      long free = rt.freeMemory();
+      long used = total - free;
 
-	@GetMapping("/cpu/{power}")
-	public String destroyCpu(@PathVariable int power) {
-		if(power <= 0){
-			power = 10;
-		}
+      return "#" + memoryCount++
+                      + ", Total: " + total
+                      + ", Used: " + used
+                      + ", Free: " + free;
+  }
 
-		int cores = Runtime.getRuntime().availableProcessors();
-		BigDecimal points_per_thread = POINTS.divide(BigDecimal.valueOf(cores));
+  @GetMapping("/cpu/{power}")
+  public String destroyCpu(@PathVariable int power) {
+      if(power <= 0){
+          power = 10;
+      }
 
-		StringBuilder info = new StringBuilder()
-						.append("points: ").append(POINTS)
-						.append("\n points_per_thread: ").append(points_per_thread)
-						.append("\n number_of_executions: ").append(power);
+      int cores = Runtime.getRuntime().availableProcessors();
+      BigDecimal points_per_thread = POINTS.divide(BigDecimal.valueOf(cores));
 
-		for (int i = 0; i < power; i++) {
-			Date now = new Date();
+      StringBuilder info = new StringBuilder()
+                      .append("points: ").append(POINTS)
+                      .append("\n points_per_thread: ").append(points_per_thread)
+                      .append("\n number_of_executions: ").append(power);
 
-			//high CPU usage simulation
-			MonteCarloPI monteCarloPI = new MonteCarloPI(cores, points_per_thread);
-			BigDecimal result = monteCarloPI.calculate();
-			BigDecimal pi = BigDecimal.valueOf(Math.PI);
-			double error = 100 * Math.abs(result.doubleValue() - Math.PI) / Math.PI;
+      for (int i = 0; i < power; i++) {
+          Date now = new Date();
 
-			info.append("\n\n pi: ").append(result.toPlainString())
-					.append("\n terror: ").append(error)
-					.append("\n in:").append( (System.currentTimeMillis() - now.getTime()) );
+          //high CPU usage simulation
+          MonteCarloPI monteCarloPI = new MonteCarloPI(cores, points_per_thread);
+          BigDecimal result = monteCarloPI.calculate();
+//          BigDecimal pi = BigDecimal.valueOf(Math.PI);
+          double error = 100 * Math.abs(result.doubleValue() - Math.PI) / Math.PI;
 
-		}
+          info.append("\n\n pi: ").append(result.toPlainString())
+                  .append("\n terror: ").append(error)
+                  .append("\n in:").append( (System.currentTimeMillis() - now.getTime()));
+      }
 
-		return info.toString();
-	}
+      return info.toString();
+  }
+  
 }
-
